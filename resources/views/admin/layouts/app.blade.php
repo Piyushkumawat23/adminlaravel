ab553c94a30b5d712fa6bcadde151ffcba41aa5b
Piyush <mailto:deepak.tanwar@icon-solutions.us>
2025-01-26 12:34:19 +0530

    sd

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -33,8 +33,8 @@
             
         </div>
             <div class="logo-name">
-                <a href="">
-                    <span>{{ $websiteSetting->site_name ?? 'Laravel 11 Multi Auth' }}</span>
+                <a target="_blank" href="{{ $websiteSetting->site_url ?? 'Laravel 11 Multi Auth' }}">
+                    <span>{{ $websiteSetting->site_name ?? 'WEBSIT' }}</span>
                 </a>
             </div>
        
